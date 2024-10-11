35832af4f5016cd068275db56eb8bc890f7e3446
Guilherme Lazani Lopes <guilhermelazarinilopes@gmail.com>
2024-10-11 16:07:07 -0300

    Add Errors

---
+++ Bookify.Domain/Abstractions/Error.cs
@@ -0,0 +1,8 @@
+﻿namespace Bookify.Domain.Abstractions;
+
+public class Error(string Code, string Name)
+{
+    public static Error None = new Error(string.Empty, string.Empty);
+
+    public static Error NullValue = new("Error.NullValue", "Null value was provided");
+}
