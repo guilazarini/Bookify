965fdda46d7e9a6b391dc215e77a1d24e4c6c8c4
Guilherme Lazani Lopes <guilhermelazarinilopes@gmail.com>
2024-10-10 22:35:28 -0300

    Add Repositorys

---
+++ Bookify.Domain/Apartaments/IApartamentsRepository.cs
@@ -0,0 +1,8 @@
+﻿using Bookify.Domain.Entities;
+
+namespace Bookify.Domain.Apartaments;
+
+public interface IApartamentsRepository
+{
+   Task<Apartment?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+}
