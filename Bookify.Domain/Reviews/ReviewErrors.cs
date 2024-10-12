87c6e02d83ac2a227738e47f8d86f83b18630090
Guilherme Lazani Lopes <guilhermelazarinilopes@gmail.com>
2024-10-12 10:53:30 -0300

    Add Reviews

---
+++ Bookify.Domain/Reviews/ReviewErrors.cs
@@ -0,0 +1,10 @@
+﻿using Bookify.Domain.Abstractions;
+
+namespace Bookify.Domain.Reviews;
+
+public static class ReviewErrors
+{
+    public static readonly Error NotEligible = new(
+        "Revisão.NãoEligível",
+        "A avaliação não é elegível porque a reserva ainda não foi concluída");
+}
